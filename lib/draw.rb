e28cd88b529b499e58ae12038393b99ed9771d77
CWEDev <CWEbrDev@gmail.com>
2020-06-19 22:28:50 -0500

    Illustration + outer boarder

--- lib/draw.rb
+++ lib/draw.rb
@@ -17,6 +17,11 @@ module DrawMethods
   TOP = {:l => "\u250C", :m => "\u252C", :r => "\u2510"}
   @@mid = {:l => "\u251C", :m => @@v_ln, :r => "\u2524"}
   BTM = {:l => "\u2514", :m => "\u2534", :r => "\u2518"}
+
+  E_TOP = {:l => "\u250F", :m => "\u2501", :r => "\u2513"}
+  @@e_mid = {:l => "\u2503", :m => " ", :r => "\u2503"}
+  E_BTM = {:l => "\u2517", :m => "\u2501", :r => "\u251B"}
+
   H_LN = " "
 
   def prints input
@@ -34,11 +39,30 @@ module DrawMethods
     output = []
     size = board.length - 1
 
-    board.each_with_index do |row, y|
-      line = y
-      gap = line + 1
+    output[0] = E_TOP[:l]
+    (board.length * 2 + 8).times do
+      output[0] += E_TOP[:m]
+    end
+    output[0] += E_TOP[:r]
 
-      output[line] = ""
+    output[1] = "#{@@e_mid[:l]}     "
+    for col in 1..board.length do
+      if col.odd?
+        output[1] += "#{col}" + " " * (4 - col.to_s.length)
+      end
+    end
+    output[1] += " #{@@e_mid[:r]}"
+
+    output[2] = @@e_mid[:l]
+    (board.length * 2 + 8).times do
+      output[2] += @@e_mid[:m]
+    end
+    output[2] += @@e_mid[:r]
+
+    board.each_with_index do |row, y|
+      line = y + 3
+
+      output[line] = "#{@@e_mid[:l]} #{(y + 65).chr}   "
 
       row.each_with_index do |pos, x|
         if pos.nil?
@@ -68,7 +92,23 @@ module DrawMethods
           end
         end
       end
+      output[line] += "    #{@@e_mid[:r]}"
     end
+
+    output.push(@@e_mid[:l])
+    (board.length * 2 + 8).times do
+      output[-1] += @@e_mid[:m]
+    end
+    output[-1] += @@e_mid[:r]
+
+    output.push(output[-1])
+
+    output.push(E_BTM[:l])
+    (board.length * 2 + 8).times do
+      output[-1] += E_BTM[:m]
+    end
+    output[-1] += E_BTM[:r]
+
     return output
   end
 end
